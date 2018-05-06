e2461925e5c03c031f42e142d10417977f68ccf4
lcc <362687201@qq.com>
2018-05-07 00:38:51 +0800

    mybatis二级缓存扩展redis存储

---
+++ zww-learn/src/main/java/com/zww/learn/controller/RedisController.java
@@ -0,0 +1,30 @@
+package com.zww.learn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zww.learn.config.RedisClient;
+
+@RestController 
+public class RedisController {
+	
+	@Autowired  
+     private RedisClient redisClinet;
+
+	 @RequestMapping("/set")  
+	 public String set(String key, String value) throws Exception{  
+		 for (long i=0;i<5000000;i++) {
+			 redisClinet.ge
+			 redisClinet.set(key+i, value+i);  
+		 }
+	        return "success";  
+	 }  
+	      
+	  @RequestMapping("/get")  
+	  public String get(String key) throws Exception {  
+	    return redisClinet.get(key);  
+	  }  
+
+
+}
